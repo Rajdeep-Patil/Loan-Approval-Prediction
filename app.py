9b6f32f72fb3d8a2c0327ba2d9fc9f7f3cef60c6
Rajdeep-Patil <rajdeepgurjar07@gmail.com>
2026-01-13 19:43:11 +0530

    final

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def predict():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5001))  # Render sets PORT dynamically
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
 
